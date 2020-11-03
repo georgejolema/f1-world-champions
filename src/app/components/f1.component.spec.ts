f4b85e3d29737adf0b91b68c01dfdf7bbe8ed774
jorge <georgejolema@gmail.com>
2020-11-03 21:28:55 +0100

    show code coverage

--- src/app/components/f1.component.spec.ts
+++ src/app/components/f1.component.spec.ts
@@ -1,16 +1,36 @@
-import { TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './f1.component';
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-      ],
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+  let storeStub: any;
+
+  beforeEach(() => {
+    storeStub = {
+      select: jasmine.createSpy().and.returnValues(of({}), of({})),
+    };
+
+    TestBed.configureTestingModule({
+      imports: [],
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+      providers: [{
+        provide: Store,
+        useValue: storeStub,
+      }],
+      schemas: [NO_ERRORS_SCHEMA],
+    });
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
-
+  it('should load the component correctly', () => {
+    expect(component).not.toBeUndefined();
+  });
 });
